d49e2117ad83e4dbb06e62c91e0822e360e988cf
mindt102 <minhdt24082002@gmail.com>
2022-04-01 22:26:03 +0700

    update main

--- main.sh
+++ main.sh
@@ -6,14 +6,26 @@ dir="NSFinalProject"
 # Set the parameters 
 nNodes=7
 packetSize=512
+serverNode=$(echo $(($nNodes / 2)))
+maxPackets=10
+interval=1
 verbose="false"
 pcap="false"
 collectData="true" # Set collect data to true to run the experiment with 2 to 30 nodes
-param="--nNodes=$nNodes --verbose=$verbose --pcap=$pcap --collectData=$collectData --packetSize=$packetSize"
+param="--nNodes=$nNodes --verbose=$verbose --pcap=$pcap --collectData=$collectData --packetSize=$packetSize --maxPackets=$maxPackets --interval=$interval"
+
+storeDir="/svmid-ps$packetSize"
 
 if [ "$collectData" == "false" ]
 then
 echo "Running $file with $nNodes nodes...";
+storeDir="/"
+fi
+
+if [ -d $dir/flowData$storeDir -a $collectData == "true" ]
+then
+	echo "Experiment has already been done. Results are stored at $storeDir."
+	exit 0
 fi
 
 # Copy the  file to scratch and run
@@ -21,9 +33,9 @@ cp $dir/$file.cc scratch/
 ./waf --run "scratch/$file $param"
 
 # Create folder to store data about flows 
-mkdir -p $dir/flowData
-mkdir -p $dir/animData
+mkdir -p $dir/flowData$storeDir
+mkdir -p $dir/animData$storeDir
 
-# Store the data in the flowData folder 
-mv final*.xml $dir/flowData
-mv anim*.xml $dir/animData
+# Store the data in the flowData$storeDir folder 
+mv final*.xml $dir/flowData$storeDir
+mv anim*.xml $dir/animData$storeDir
